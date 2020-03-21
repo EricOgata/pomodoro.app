838dc304248642c7f27dd4301650a64675de04d9
Eric Ogata - WebHub <eric.ogata@webhub.com.br>
2020-03-20 23:32:38 -0300

    [ INITIAL ] ESlint & Organization

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../../images/logo.svg';
 import './App.css';
 
-function App() {
+function Index() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/Index.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -23,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+export default Index;
